545b54ae202316ce91df0a3eb3a9e570e725622f
Jhapendra kandel <jhapendrakandel10@gmail.com>
2026-01-19 17:54:27 +0545

    Add simple startup message in main.py

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ ip_blacklist = []  # e.g., ['192.168.1.100']
 
 if __name__ == "__main__":
    
+    print("NetGuard DNS Monitor v" + __version__)
+    print("Starting DNS server and GUI...")
+
     dns_thread = threading.Thread(target=start_dns_server, args=(log_queue, all_logs, domain_blocklist, ip_blacklist), daemon=True)
     dns_thread.start()
 
